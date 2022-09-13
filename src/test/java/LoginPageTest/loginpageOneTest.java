9f76c70db73fe37557000daf2d8ecedff8ead713
srigowri98 <gowri6498@gmail.com>
2022-09-13 14:37:09 +0530

    2nd commit from 2nd workspace

--- src/test/java/LoginPageTest/loginpageOneTest.java
+++ src/test/java/LoginPageTest/loginpageOneTest.java
@@ -3,6 +3,7 @@ package LoginPageTest;
 public class loginpageOneTest {
 	public static void main(String[] args) {
 		System.out.println("login page is displayed");
+		System.out.println(" login page 2 is displayed");
 	}
 
 }
